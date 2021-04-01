402834adb2734835b7d0620033ce02dfb0a8d25d
louisyu <louisyulin511@gmail.com>
2021-04-01 18:19:29 +0800

    fix: 修复录音无法启动的问题 删除无用的日志打印 调整代码格式

--- ios/Chivox/RNChivox.m
+++ ios/Chivox/RNChivox.m
@@ -1,99 +1,98 @@
 #import "RNChivox.h"
 #import "CAIEngine.h"
 
-static NSString *const ChivoxEventNotification = @"cn.bookln.chivox.notification.event";
-static NSString *const ChivoxEventNotificationBodyType = @"cn.bookln.chivox.notification.event.bodyType";
-static NSString *const ChivoxEventNotificationBodyValue = @"cn.bookln.chivox.notification.event.bodyValue";
-static NSString *const GET_CHIVOX_DATA_NOTIFICATION = @"cn.bookln.oneClickLogin.getChivoxDataNotification";
-
+static NSString *const ChivoxEventNotification = @"cn.bookln.oneClickLogin.getChivoxDataNotification";
 
 @interface RNChivox()
-//问题
-@property (nonatomic, assign) ChivoxAIEngine* cloudengine;
 @end
 
 @implementation RNChivox
+{
+ ChivoxAIEngine* cloudengine;
+}
 
 RCT_EXPORT_MODULE(ChivoxModule)
-RCT_EXPORT_METHOD(initChivoxSdk:(NSString *)appKey secretKey:(NSString *)secretKey){
-    // 构建配置信息
+
+- (NSArray<NSString *> *)supportedEvents
+{
+    return @[ChivoxEventNotification];
+}
+
+RCT_EXPORT_METHOD(initChivoxSdk:(NSString *)appKey secretKey:(NSString *)secretKey)
+{
+        // 构建配置信息
         NSMutableDictionary *cfg = [[NSMutableDictionary alloc] init]; //设置传入参数
         NSMutableDictionary *cloud = [[NSMutableDictionary alloc] init];//设置cloud参数
-    //授权信息
+        //授权信息
         [cfg setObject:appKey forKey:@"appKey"];
         [cfg setObject:secretKey forKey:@"secretKey"];
         NSString * provision = [[NSBundle mainBundle] pathForResource:@"aiengine" ofType:@"provision"];
         [cfg setObject:provision forKey:@"provision"];
-    
-    //网络连接
+        //网络连接
         [cloud setObject:@1 forKey:@"enable"];
-    //传入参数为nsnumber类型，是@0，不是@"0"
+        //传入参数为nsnumber类型，是@0，不是@"0"
         [cloud setObject:@1 forKey:@"protocol"];
         [cloud setObject:@5 forKey:@"connectTimeout"];
         [cloud setObject:@10 forKey:@"serverTimeout"];
-    //初始化网络信息
+        //初始化网络信息
         cfg[@"cloud"] = cloud;
-    //引擎回调
+        //引擎回调
         ChivoxAIEngineCreateCallback *cb = [ChivoxAIEngineCreateCallback
               onSuccess:^(ChivoxAIEngine * _Nonnull engine) {
-            self.cloudengine = engine;
-            NSLog(@"success: %@",engine);
-        } onFail:^(ChivoxAIRetValue * _Nonnull err) {
-            NSLog(@"fail: %@",err);
-        }];
+            self->cloudengine = engine;
+            
+        } onFail:^(ChivoxAIRetValue * _Nonnull err) {}];
         //初始化
         [ChivoxAIEngine create:cfg cb:cb];
-    
+        //设置AVAudioSession Category为可录音的选项
+        AVAudioSession* sharedSession = [AVAudioSession sharedInstance];
+        [sharedSession setCategory:AVAudioSessionCategoryRecord error:nil];
 }
 
 RCT_EXPORT_METHOD(startChivoxRecord:(nonnull NSDictionary *)options resolve:(RCTPromiseResolveBlock)resolve
-    reject:(RCTPromiseRejectBlock)reject){
+    reject:(RCTPromiseRejectBlock)reject)
+{
+    if(nil == self->cloudengine){
+        reject(@"0",@"chivox初始化失败", nil);
+    }
     
     NSString *coreType = options[@"coreType"];
     NSString *refText = options[@"refText"];
-    NSString *attachAudioUrl = options[@"attachAudioUrl"];
-    //问题
-    if(!coreType || coreType.length == 0 || !refText || refText.length ==0 || !attachAudioUrl || attachAudioUrl.length == 0){
-        //问题
+    NSNumber *attachAudioUrl = options[@"attachAudioUrl"];
+    if(!coreType || coreType.length == 0 || !refText || refText.length ==0 || !attachAudioUrl){
         reject(@"0",@"操作失败", nil);
         return;
     }
-    
-    ChivoxAIEvalResultListener *handler = [[ChivoxAIEvalResultListener alloc] init];//创建监听对象
+    //创建监听对象
+    ChivoxAIEvalResultListener *handler = [[ChivoxAIEvalResultListener alloc] init];
+    //完成的结果回调
     handler.onEvalResult = ^(NSString * _Nonnull eval, ChivoxAIEvalResult *
             _Nonnull result) {
         NSLog(@"result:%@ path:%@",result.text,result.recFilePath);
         if (result.text == nil) {
-            NSLog(@"返回结果空！！！");
-            reject(@"0",@"操作失败", nil);
+            reject(@"0",@"返回结果空！！！", nil);
             return;
         }
-        //问题
-        NSMutableDictionary *body = [[NSMutableDictionary alloc] init];
-        [body setObject:result.text forKey:@"data"];
-        [body setObject:@true forKey:@"success"];
-        [self sendEventWithName:ChivoxEventNotificationBodyType body:body];
-        
+        NSDictionary *body = @{@"data":result.text,@"success":@(true)};
+        [self sendEventWithName:ChivoxEventNotification body:body];
     };
-    
+    //错误回调
     handler.onError = ^(NSString * _Nonnull eval, ChivoxAIEvalResult *
             _Nonnull result) {
-        NSLog(@"error:%@111111",result.text);
-        NSMutableDictionary *body = [[NSMutableDictionary alloc] init];
-        [body setObject:@false forKey:@"success"];
-        [self sendEventWithName:ChivoxEventNotificationBodyType body:body];
+        NSDictionary *body = @{@"data":@"onError:----",@"success":@(false)};
+        [self sendEventWithName:ChivoxEventNotification body:body];
     };
-
-    ChivoxAIInnerRecorder *innerRecorder = [[ChivoxAIInnerRecorder alloc] init];//创建音频源
+    //创建音频源
+    ChivoxAIInnerRecorder *innerRecorder = [[ChivoxAIInnerRecorder alloc] init];
     innerRecorder.recordParam.channel = 1;
     innerRecorder.recordParam.sampleRate = 16000;
     innerRecorder.recordParam.sampleBytes = 2;
-    /*获取本地播放文件路径*/
+    //获取本地播放文件路径
     NSString *device_id = [ChivoxAIEngine getDeviceId];
     NSString *filename = [[NSString alloc] initWithFormat:@"%@/Documents/record/%@.wav",NSHomeDirectory(),device_id];
     innerRecorder.recordParam.saveFile = filename;
-    
-    NSMutableDictionary *param = [[NSMutableDictionary alloc] init];//创建测评参数
+    //创建测评参数
+    NSMutableDictionary *param = [[NSMutableDictionary alloc] init];
     NSMutableDictionary *audio = [[NSMutableDictionary alloc] init];
     [param setObject:@1 forKey:@"soundIntensityEnable"];
     [param setObject:@"cloud" forKey:@"coreProvideType"];
@@ -113,7 +112,6 @@ RCT_EXPORT_METHOD(startChivoxRecord:(nonnull NSDictionary *)options resolve:(RCT
     [request setObject:attachAudioUrl forKey:@"attachAudioUrl"];
     [param setObject:request forKey:@"request"];
     param[@"coreProvideType"] = @"cloud";
-    NSLog(@"%@", param);
     
     ChivoxAIRetValue * e  = nil;
     [ChivoxAIRecorderNotify sharedInstance].onRecordStart = ^{
@@ -122,33 +120,28 @@ RCT_EXPORT_METHOD(startChivoxRecord:(nonnull NSDictionary *)options resolve:(RCT
     [ChivoxAIRecorderNotify sharedInstance].onRecordStop = ^{
         NSLog(@"stop recorder");
     };
-
-    
-    NSMutableString *tokenid = [[NSMutableString alloc] init];//tokenid传入 获取
-    e = [self.cloudengine start:innerRecorder tokenId:tokenid param:param listener:handler]; //开始评测
+   
+    NSMutableString *tokenid = [[NSMutableString alloc] init];
+    e = [self->cloudengine start:innerRecorder tokenId:tokenid param:param listener:handler]; //开始评测
     if (0 != [e errId]){
-        NSLog(@"失败原因：%@",e);//打印失败原因
-        reject(@([e errId]).stringValue,@"操作失败", nil);
+        reject(@([e errId]).stringValue,@"startChivoxRecord操作失败", nil);
     }else{
-        //问题
         resolve(nil);
     }
 }
 
 RCT_EXPORT_METHOD(stopChivoxRecord:(RCTPromiseResolveBlock)resolve
-                                    reject:(RCTPromiseRejectBlock)reject){
-    
-    if(self.cloudengine == nil){
-        reject(@"0",@"操作失败", nil);
+                                    reject:(RCTPromiseRejectBlock)reject)
+{
+    if(self->cloudengine == nil){
+        reject(@"0",@"stopChivoxRecord操作失败", nil);
         return;
     }
     ChivoxAIRetValue * e  = nil;
-    e = [self.cloudengine stop];
+    e = [self->cloudengine stop];
     if (0 != [e errId]){
-        NSLog(@"失败原因：%@",e);//打印失败原因
-        reject(@([e errId]).stringValue,@"操作失败", nil);
+        reject(@([e errId]).stringValue,@"stopChivoxRecord操作失败", nil);
     }else{
-        //问题
         resolve(nil);
     }
     
